67a0fd27db1dd553adda49ad6639197c509f0eb4
joaotrog1 <joaogustavotrog@gmail.com>
2025-09-18 11:12:13 -0300

    Att

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
 </head>
 <body>
     <header>
-        <h1><span>HSXITS</span>@yield('titulo')</h1>
+        <h1><span>ENCURTADOR</span>@yield('titulo')</h1>
         @yield('btn')
         
     </header>
